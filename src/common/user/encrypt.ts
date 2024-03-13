7038d971ed2574e2b80289533fff78dbb22549ae
式部 <qq_36105691@noreply.gitcode.com>
2024-03-13 15:36:08 +0800

    fix

--- src/common/user/encrypt.ts
+++ src/common/user/encrypt.ts
@@ -14,7 +14,7 @@ export async function rsaEncrypt(dataToEncrypt: string): Promise<string> {
     }
 
     const pemKey = `-----BEGIN PUBLIC KEY-----\n${publicKey}\n-----END PUBLIC KEY-----`;
-    const forge = require('node-forge');
+    const forge = await import('node-forge');
     const publicKeyObj = forge.pki.publicKeyFromPem(pemKey);
     const encrypted = publicKeyObj.encrypt(dataToEncrypt, 'RSA-OAEP', {
         md: forge.md.sha256.create(),
