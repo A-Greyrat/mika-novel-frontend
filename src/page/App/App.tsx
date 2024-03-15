0d691935c70ad7f435c44c07d5a322021991039a
式部 <qq_36105691@noreply.gitcode.com>
2024-03-15 12:39:08 +0800

    fix

--- src/page/App/App.tsx
+++ src/page/App/App.tsx
@@ -1,6 +1,7 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import React, {Suspense} from "react";
 import LoadingPage from "../Loading/LoadingPage";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const Home = React.lazy(() => import('../Home/Home'));
 const Login = React.lazy(() => import('../Login/Login'));
@@ -72,7 +73,9 @@ const router = createBrowserRouter([
 
 const App = () => {
     return (
+
         <Suspense fallback={<LoadingPage/>}>
+        <SpeedInsights />
             <RouterProvider router={router}/>
         </Suspense>
     )
